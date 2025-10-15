6345d78c769b0ee8b8749285be4765d5b2adff7b
shruti0441 <shrutiadsul04@gmail.com>
2025-10-16 00:55:26 +0530

    setup

--- README.md
+++ README.md
@@ -3,6 +3,13 @@
 ''' bash
 conda create -n mlproj python=3.8  -y
 '''
+
+'''bash
+conda activate mlproj
+'''
+
 ''' bash 
 pip instal -r requierments.txt
 '''
+
+
--- setup.py
+++ setup.py
@@ -17,5 +17,13 @@ steuptools.setup(
     email=AUTHOR_EMAIL,
     decription = "A small python package for  ml app",
     long_discription =long_discription,
+    long_discription_content = "text/markdown",
+    url = f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "bug_tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issue"
+
+    },
+    package_dir = {"":"src"},
+    packages =steuptools.find_packages(where = 'src')
 
 )
