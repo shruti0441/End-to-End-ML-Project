00cf91725c6acb2f8b364b79ce2f29c33e224c3a
shruti0441 <shrutiadsul04@gmail.com>
2025-10-15 19:10:49 +0530

    requirements

---
+++ setup.py
@@ -0,0 +1,21 @@
+import steuptools
+
+with open("README.md",'r' ,encoding='utf-8') as f :
+    long_discription =f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME= "End-to-End-ML-Project"
+AUTHOR_USER_NAME = "shruti0441"
+SRC_REPO = "mlproject"
+AUTHOR_EMAIL = "shrutiadsul04@gmail.com"
+
+steuptools.setup(
+    name = SRC_REPO,
+    version=__version__,
+    author= AUTHOR_USER_NAME,
+    email=AUTHOR_EMAIL,
+    decription = "A small python package for  ml app",
+    long_discription =long_discription,
+
+)
--- templates/setup.py
+++
@@ -1,8 +0,0 @@
-import steuptools
-
-with open("README.md",'r' ,encoding='utf-8') as f :
-    long_discription =f.read()
-
-__version__ = "0.0.0"
-
-
